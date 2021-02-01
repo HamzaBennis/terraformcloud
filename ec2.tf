89bf5163510a21cc0f31008e16438d79acae2660
Hamza Bennis <61824238+HamzaBennis@users.noreply.github.com>
2021-02-01 15:07:11 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,19 +1,13 @@
 provider "aws" {
   region     = "us-east-1"
-
 }
 
 resource "aws_instance" "myec2" {
   ami           = "ami-0083662ba17882949"
-  instance_type = lookup(var.instance_type, terraform.workspace)
-}
-
-variable "instance_type" {
-  type = map(any)
-
-  default = {
-    default = "t2.nano"
-    dev     = "t2.micro"
-    prod    = "t2.large"
+  instance_type = "t2.micro"
+  key_name      = "devops-dirane"
+  tags = {
+    Name = "ec2-dirane"
   }
 }
+
